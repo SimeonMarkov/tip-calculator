5516df2e97944aaef3aaa8a6c94a2fbda4e22140
Simeon Markov <simeon.markov1994@gmail.com>
2026-04-15 12:13:05 +0300

    removed a bug when switching from predefined to inserted custom tip

--- src/FormComponent.tsx
+++ src/FormComponent.tsx
@@ -1,5 +1,5 @@
 import './FormComponent.css';
-import {useEffect, useRef, useState} from "react";
+import {useRef, useState} from "react";
 import * as React from "react";
 
 export default function FormComponent() {
@@ -7,8 +7,6 @@ export default function FormComponent() {
     const tips: string[] = ['5%', '10%', '15%', '25%', '50%']
     const [bill, setBill] = useState<string>('0.00');
     const [numberOfPeople, setNumberOfPeople] = useState<number>(0);
-    const [tipPerPerson, setTipPerPerson] = useState<string>('0.00');
-    const [totalPerPerson, setTotalPerPerson] = useState<string>('0.00');
     const [tipSelected, setTipSelected] = useState<number>(0);
 
     const customTipRef = useRef<HTMLInputElement>(null);
@@ -37,20 +35,6 @@ export default function FormComponent() {
         document.querySelector('.selected')?.classList.remove("selected");
     }
 
-    useEffect(() => {
-        const interval = setTimeout(() => {
-            if (numberOfPeople) {
-
-                setTipPerPerson((Number(bill) * tipSelected / 100 / numberOfPeople).toFixed(2));
-                setTotalPerPerson(((Number(bill) * (1 + tipSelected / 100)) / numberOfPeople).toFixed(2));
-            } else {
-                setTipPerPerson('0.00');
-                setTotalPerPerson('0.00');
-            }
-        }, 50);
-        return () => clearTimeout(interval);
-    }, [bill, numberOfPeople, tipSelected]);
-
     return (
         <form>
             <div className="bill-container">
@@ -63,8 +47,10 @@ export default function FormComponent() {
             <section className="tip-percentage-container">
                 <h4>Select Tip %</h4>
                 <div className="tip-percentage-grid">
-                    {tips.map(tip => (<span key={tip} className="tip-control predefined-tip-percentage" onClick={handleTipSelect}>{tip}</span>))}
-                    <input className="tip-control custom-tip-control" size="4" placeholder="Custom" ref={customTipRef} onClick={handleTipSelect} onInput={(e) => setTipSelected(e.target.value)} />
+                    {tips.map(tip => (<span key={tip} className={`tip-control predefined-tip-percentage ${tipSelected === Number(tip.slice(0, tip.length - 1)) ? 'selected' : ''}`} onClick={handleTipSelect}>{tip}</span>))}
+                    <input className={`tip-control custom-tip-control ${
+                        !tips.includes(`${tipSelected}%`) && tipSelected !== 0 ? 'selected' : ''
+                    }`} size={4} placeholder="Custom" ref={customTipRef} onChange={(e) => setTipSelected(e.target.value)} />
                 </div>
             </section>
 
@@ -79,11 +65,11 @@ export default function FormComponent() {
             <section className="total-summary">
                 <div className="summary-row">
                     <div>Tip Amount<span className="per-person"></span></div>
-                    <span className="money-amount" ref={tipPerPersonRef}>&#36;{tipPerPerson}</span>
+                    <span className="money-amount" ref={tipPerPersonRef}>&#36;{numberOfPeople ? (Number(bill) * tipSelected / 100 / numberOfPeople).toFixed(2) : '0.00'}</span>
                 </div>
                 <div className="summary-row">
                     <div>Total<span className="per-person"></span></div>
-                    <span className="money-amount" ref={totalPerPersonRef}>&#36;{totalPerPerson}</span>
+                    <span className="money-amount" ref={totalPerPersonRef}>&#36;{numberOfPeople ? ((Number(bill) * (1 + tipSelected / 100)) / numberOfPeople).toFixed(2) : '0.00'}</span>
                 </div>
                 <span className="btn" onClick={handleReset}>Reset</span>
             </section>
